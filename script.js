cdb71ea841df3fa4b89cf341a7ce253f9f96e5e6
Diamond M <89270895+DMill20@users.noreply.github.com>
2022-02-16 06:29:15 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
     hour = minute * 60,
     day = hour * 24;
 
-  let christmas = "Dec 25, 2021 00:00:00",
+  let christmas = "Dec 25, 2022 00:00:00",
     countDown = new Date(christmas).getTime(),
     x = setInterval(function () {
       let now = new Date().getTime(),
